09ab5191fc74d3cb16008e3d83034daf7a753fab
Eleanor Skinner <eleanors@gmx.com>
2022-10-21 17:27:29 +0100

    Starting blank program structure

---
+++ Task1/main.c
@@ -0,0 +1,27 @@
+/*
+ * Task1.c
+ *
+ * Created: 21/10/2022 17:07:38
+ * Author : Eleanor
+ */ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// Set up pin states and values, and initialize any internal hardware which
+// has a consistent use in the program.
+void init()
+{
+	
+}
+
+int main(void)
+{
+	// Run initialization function
+	init();
+	// Main Program Loop
+    while (1) 
+    {
+    }
+}
+
